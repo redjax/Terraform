13ab50515563126767807266f7ecc31e1029ddda
redjax <no@none.com>
2025-04-20 22:07:55 -0400

    Fix s3 backend  url

--- environments/cloudflare/backend.tf
+++ environments/cloudflare/backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket                      = "terraform-git-repo"
-    key                         = "terraform.tfstate"
-    region                      = "us-west-002"
-    endpoint                    = "s3.us-west-002.backblazeb2.com"
+    bucket = "terraform-git-repo"
+    key    = "terraform.tfstate"
+    region = "us-west-002"
+    endpoints = {
+      s3 = "https://s3.us-west-002.backblazeb2.com"
+    }
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
