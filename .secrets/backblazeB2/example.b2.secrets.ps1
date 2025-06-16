48f1ecf295f54a7df5cf7c201649969d58389400
redjax <5534031+redjax@users.noreply.github.com>
2025-06-16 01:37:47 -0400

    fix(powershell): Update example b2 secrets powershell script

--- .secrets/backblazeB2/example.b2.secrets.ps1
+++ .secrets/backblazeB2/example.b2.secrets.ps1
@@ -9,5 +9,11 @@
     Example:
     . "$PSScriptRoot\b2.secrets.ps1"
 #>
+
+## Backblaze B2 secrets
 $Env:AWS_ACCESS_KEY_ID = "YOUR_B2_APPLICATION_KEY_ID"
 $Env:AWS_SECRET_ACCESS_KEY = "YOUR_B2_APPLICATION_KEY"
+
+## Fixes an authentication error with Backblaze B2
+$env:AWS_REQUEST_CHECKSUM_CALCULATION = "when_required"
+$env:AWS_RESPONSE_CHECKSUM_VALIDATION = "when_required"
