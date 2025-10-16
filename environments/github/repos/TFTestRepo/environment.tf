16cac400b5c5d495fb8cb465a470f9caa7b07052
redjax <5534031+redjax@users.noreply.github.com>
2025-10-16 00:02:19 -0400

    fix(env): Add vars to gh_repo module import

--- environments/github/repos/TFTestRepo/environment.tf
+++ environments/github/repos/TFTestRepo/environment.tf
@@ -9,4 +9,10 @@ terraform {
 
 module "gh_repo" {
   source = "../../../../modules/github/Repository"
+
+  repository_name        = var.repository_name
+  repository_description = var.repository_description
+  repository_visibility  = var.repository_visibility
+  default_branch         = var.default_branch
+  additional_branches    = var.additional_branches
 }
